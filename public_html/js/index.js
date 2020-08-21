69a17b660f967fd2fa7bfad996cf07ee9edaf688
rasayesh <r.asayesh777@gmail.com>
2020-08-20 21:15:36 -0700

    modified index.html removed addAccount.html merged code with index.html

--- public_html/js/index.js
+++ public_html/js/index.js
@@ -1,24 +1,59 @@
 /**
  * @author Reza Munoz-Asayesh
  * @file index.js 
- * @project Asteroid Miners
+ * @project Asteroid Wars
  * @description 
  * 
  */
 
-/**
- * @param {N/A} 
- * @returns - N/A
- * 
- * @description This function logs a user into
- * the site.
- * 
- */
+
+$(document).ready(() => {
+
+});
+
+
+// show login div, hide directory & signup divs
+function showLogin() {
+    $('#directoryButton').css('display', 'none');
+    $('#loginContainer').css('display', 'block');
+    $('#signupContainer').css('display', 'none');
+}
+
+// show signup div, hide directory & login divs
+function showSignup() {
+    $('#directoryButton').css('display', 'none');
+    $('#loginContainer').css('display', 'none');
+    $('#signupContainer').css('display', 'block');
+}
+
+// show directory div, hide login & signup divs
+function back() {
+    $('#directoryButton').css('display', 'block');
+    $('#loginContainer').css('display', 'none');
+    $('#signupContainer').css('display', 'none');
+}
+
+
+// validate the passwords by confirming matching passwords before user creates an account
+function validatePassword() {
+    let password = $('#password').val();
+    let confirmPassword = $('#confirmPassword').val();
+    if (password == confirmPassword) {
+        createAccount();
+    } else {
+        $('#passwordDontMatchError')
+            .html('Passwords Don\'t Match')
+            .css('color', 'red');
+    }
+}
+
+
+// This function logs a user into the site, making sure information matches that in database
 function login() {
-    let username = $('#username').val(); // username_input
-    let password = $('#password').val(); // password_input
-    $('#username').val('');
-    $('#password').val('');
+    let username = $('#loginUsername').val(); // username_input
+    let password = $('#loginPassword').val(); // password_input
+    $('#loginUsername').val('');
+    $('#loginPassword').val('');
     $.ajax({
         url: '/login/' + username + '/' + password,
         data: {},
@@ -36,6 +71,20 @@ function login() {
 }
 
 
-function goToCreateAccount() {
-    window.location = '../create_account.html'; // keeps history.
+// This function adds a user to the database by taking inputs of username and password 
+// and sending them over to the server, via JSON format.
+function createAccount() {
+    let un = $('#username').val();
+    let pw = $('#password').val();
+    $('#username').val('');
+    $('#password').val('');
+    $.ajax({
+        url: '/add/user/',
+        data: { username: un, password: pw },
+        method: 'POST',
+        success: function(output) {
+            alert(output);
+            back();
+        }
+    });
 }
